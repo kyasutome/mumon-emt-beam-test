e256a289ce2c360a55f3d6b7b0c0cdf8ca70a899
kyasutome <yasutome.kenji.38r@st.kyoto-u.ac.jp>
2020-11-03 23:57:58 +0900

    conflict

--- peacock/matusada_power/power_off.sh
+++ peacock/matusada_power/power_off.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 #!/usr/bin/bash
 /Users/kenji/Documents/mumon-git/mumon-emt-beam-test-hub/peacock/matusada_power/power_OFF
-=======
-#!/bin/bash
-./power_OFF
->>>>>>> dd6a8917c130bd844752015986874ff1f0897a52
--- peacock/matusada_power/power_on.sh
+++ peacock/matusada_power/power_on.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 #!/usr/bin/bash
 /Users/kenji/Documents/mumon-git/mumon-emt-beam-test-hub/peacock/matusada_power/power_ON
-=======
-#!/bin/bash
-./power_ON
->>>>>>> dd6a8917c130bd844752015986874ff1f0897a52
